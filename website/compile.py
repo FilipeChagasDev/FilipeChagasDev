95c313b41616c25510628e30259abc6a62248c4b
Filipe Chagas <filipe.ferraz0@gmail.com>
2024-11-03 00:56:29 +0000

    Website content listing update

--- website/compile.py
+++ website/compile.py
@@ -1,23 +1,36 @@
 from jinja2 import Environment, FileSystemLoader
 import os
- 
+import json
+from datetime import datetime
+
 root = os.path.dirname(os.path.abspath(__file__))
-templates_dir = os.path.join(root, 'templates')
-env = Environment( loader = FileSystemLoader(templates_dir) )
-template = env.get_template('index.html')
- 
-filename = os.path.join(root, 'index.html')
-with open(filename, 'w') as fh:
-    fh.write(template.render(
-        posts = [
-            {
-                'index': 0,
-                'title': 'Seam Carving',
-                'intro': 'Seam Carving is an algorithm that reduces the width or height of an image, avoiding distortions in its content. This animation shows the process of reducing the width of an image with Seam Carving: for each pixel of width to be limited, a "minimum energy" vertical path (where there is less variation in brightness) is found and its pixels are removed. In this implementation, the "energy" of the image is calculated as the magnitude of the gradient.',
-                'pic': 'https://github.com/FilipeChagasDev/FilipeChagasDev/blob/main/notebooks/seam-carving/output/seam-carving-filipe.gif?raw=true',
-                'url': 'https://github.com/FilipeChagasDev/FilipeChagasDev/blob/main/notebooks/seam-carving/seam-carving-filipe.ipynb',
-                'content_type': 'Python Notebook',
-                'content_topic': 'Image Processing'
-            }
-        ],
-    ))
+
+def read_notebooks_metadata():
+    notebooks_dir = os.path.join(root, '..', 'notebooks')
+    notebooks_meta_list = []
+
+    for notebook_dir in os.listdir(notebooks_dir):
+        meta_fn = os.path.join(notebooks_dir, notebook_dir, 'meta.json')
+        if os.path.exists(meta_fn):
+            with open(meta_fn, 'r') as f:
+                notebooks_meta_list.append(json.load(f))
+
+    return notebooks_meta_list
+
+if __name__ == '__main__':
+    templates_dir = os.path.join(root, 'templates')
+    env = Environment(loader = FileSystemLoader(templates_dir))
+    template = env.get_template('index.html')
+    
+    posts = read_notebooks_metadata()
+    
+    posts.sort(
+            key=lambda meta: (meta['relevance'], datetime(meta['date']['year'], meta['date']['month'], meta['date']['day']))
+        )
+    
+    for meta in posts:
+        meta['date_str'] = datetime(meta['date']['year'], meta['date']['month'], meta['date']['day']).strftime('%d-%b-%Y')
+
+    filename = os.path.join(root, 'index.html')
+    with open(filename, 'w') as fh:
+        fh.write(template.render(posts = posts))
